63a45985ac9bda1da018a3c5d2b6fb178e850e1c
rafikidota <lesmes240698@gmail.com>
2022-10-06 19:06:27 -0400

    course factory

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -15,24 +15,6 @@ class CourseSeeder extends Seeder
      */
     public function run()
     {
-        $course1 = new Course();
-        $course2 = new Course();
-        $course3 = new Course();
-
-        $course1->name = "Laravel 1";
-        $course2->name = "Laravel 2";
-        $course3->name = "Laravel 3";
-
-        $course1->description = "Best PHP Framework";
-        $course2->description = "Best PHP Framework";
-        $course3->description = "Best PHP Framework";
-
-        $course1->category = "Web Development";
-        $course2->category = "Web Development";
-        $course3->category = "Web Development";
-
-        $course1->save();
-        $course2->save();
-        $course3->save();
+        Course::factory(50)->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(LaratrustSeeder::class);
-        $this->call(CourseSeeder::class);
+        $this->call([
+            LaratrustSeeder::class,
+            CourseSeeder::class
+        ]);
     }
 }
